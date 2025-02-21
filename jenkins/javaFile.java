aa50818dab49e795566243ead64087a810e2f06c
Reddy <bsuryateja777@gmail.com>
2025-02-21 12:59:23 +0530

    Commit test

--- jenkins/javaFile.java
+++ jenkins/javaFile.java
@@ -1,5 +1,5 @@
 class javaFile{
     public static void main(String args[]){
-        System.out.println("Hello jenkins free-style job");
+        System.out.println("Hello jenkins free-style");
     }
 }
